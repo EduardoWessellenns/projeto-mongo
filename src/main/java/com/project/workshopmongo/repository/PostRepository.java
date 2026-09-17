28a22f91885168355376a1476d8bb05519a355b4
Eduardo Wessellenns <eduardowessellennss@gmail.com>
2026-09-17 17:08:49 -0300

    Criando entity Post com user aninhado (corrigir, post nao esta indo para o banco de daods)

---
+++ src/main/java/com/project/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.project.workshopmongo.repository;
+
+import com.project.workshopmongo.domain.Post;
+import com.project.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
